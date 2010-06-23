7aa7bb813dcf815cdd8f360c68681534ba5fbae7
pepijndevos <pepijndevos@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-23 15:00:53 +0000

    fix tag
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-rss-shortcode/trunk@256501 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wordpress-rss-shortcode.php
+++ wordpress-rss-shortcode.php
@@ -4,7 +4,7 @@ Plugin Name: Wordpress RSS shortcode
 Plugin URI: http://pepijndevos.nl/2010/06/wordpress-rss-widget-in-a-post-page/
 Description: This plugin fetches an RSS feed as a shortcode
 Author: Pepijn de Vos
-Version: 1.0
+Version: 1.1
 Author URI: http://pepijndevos.nl
 */
 
@@ -16,7 +16,11 @@ function rsstag($atts) {
 		'show_summary' => 0
 	), $atts);
 	//var_dump($atts);
+	ob_start();
 	wp_widget_rss_output($atts['url'], $atts);
+	$rss = ob_get_contents();
+	ob_end_clean();
+	return $rss;
 }
 add_shortcode('rsstag', 'rsstag');
 ?>
